052f4f83db53c84489e499d0934ebe7b40577d53
kiven <221359@qq.com>
2020-03-26 11:00:21 +0800

    1

---
+++ jwd/src/main/java/cn/jwd/JwdApplication.java
@@ -0,0 +1,13 @@
+package cn.jwd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwdApplication.class, args);
+	}
+
+}
---
+++ jwd/src/test/java/cn/jwd/JwdApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jwd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
